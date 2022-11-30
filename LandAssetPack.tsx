cc29ab9372cb669a00cb808ffbe9f6e55ef7b8b9
Navajjoo <info@lgbtqnetwork.space>
2022-11-30 19:27:45 +0200

    Delete LandAssetPack.tsx

--- LandAssetPack.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Roguelike" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
- <image source="Roguelike.png" width="968" height="526"/>
-</tileset>
